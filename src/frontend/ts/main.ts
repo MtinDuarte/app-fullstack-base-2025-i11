ed8a9b8f0d978c7d12d659f3eb56756473c79471
MtinDuarte <duartemartn@gmail.com>
2025-06-16 09:50:52 -0300

    Click event listener, html load, input element. Docker compose updated.

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,17 @@
 
-function SayHello(){
+/*  This will be executed after all HTML is loaded... */
+window.addEventListener("load" ,  () => {
+    
+    /* Grab current value of textarea_1 and cast as HTMLInputElement  */
     let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
+
+    /* Append text... */
+    let new_value = "Hola mundo !!!" + "\n" + current_value.value;
+    
+    /* Assign new value */
     document.getElementById("textarea_1").innerHTML = new_value;
-}
+
+    /* Click event listener for button checking if it is not null.. */
+    document.getElementById("btn_1")?.addEventListener("click", ()=>{alert("Se hizo click");});
+});
+
